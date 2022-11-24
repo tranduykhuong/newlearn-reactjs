85df8c292a9f02f18db596286a1a5f0bfce08a67
tranduykhuong <tranduykhuongit@gmail.com>
2022-11-25 00:16:01 +0700

    test git pull

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tran Duy Khuong
+          Tran Duy Khuong HCMUS
         </p>
         
       </header>
